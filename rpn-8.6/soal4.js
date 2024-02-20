6de9c2f73a8a006cd0cdaed7e1dcc648f25f50d8
alpharii <alphari404@gmail.com>
2024-02-20 14:45:54 +0700

    part-8.6

---
+++ rpn-8.6/soal4.js
@@ -0,0 +1,19 @@
+function tentukanDeretGeometri(arr) {
+    let geometri = arr[1] / arr[0]
+    let result = ""
+    for (let i=0; i<arr.length-1; i++){
+        result = arr[i+1]/arr[i] 
+    } if (geometri == result){
+        return true
+    } else{
+        return false
+    }
+}
+  
+  // TEST CASES
+  console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+  console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+  console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+  console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+  console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
+  
