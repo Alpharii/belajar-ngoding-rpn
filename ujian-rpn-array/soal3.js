068493bd6471bc5b6751460ce0852fdb0f0853dd
alpharii <alphari404@gmail.com>
2024-02-24 13:21:18 +0700

    add ujian-rpn-array

--- ujian-rpn-array/soal3.js
+++ ujian-rpn-array/soal3.js
@@ -1,25 +1,31 @@
 /*
 diberikan sebuah function groupAnimals(animals) yang menerima satu parameter berupa array,
 fungsi ini akan me-return array 2 dimensi
+*//*
+diberikan sebuah function groupAnimals(animals) yang menerima satu parameter berupa array,
+fungsi ini akan me-return array 2 dimensi
 */
+
 function groupAnimals(animals) {
-  let arr1 = []
-  let arr2 = []
-  let arr3 = []
-  let result = [arr1,arr2,arr3]
-    for (i=0; i<animals.length; i++){
-      if(animals[i][0] == "a"){
-        arr1.push(animals[i])
-      } else if (animals[i][0]== "c"){
-        arr2.push(animals[i])
-      } else if (animals [i][0] == "k"){
-        arr3.push(animals[i])
-      } 
-    } return result
-  }
-  
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+    animals.sort()
+    let result = []
+    let container = []
+    for(i=0; i<animals.length; i++){
+        if (i == 0){
+            container.push(animals[i])
+        } else if(animals[i][0] !== container[0][0]){
+            result.push(container)
+            container = [animals[i]]
+            console.log(animals[i])
+        } else {
+            container.push(animals[i])
+        }
+ } result.push(container)
+ console.log(result)
+}
+
+// TEST CASES
+console.log(groupAnimals(["cacing", "ayam", "kuda", "anoa", "kancil"]));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
