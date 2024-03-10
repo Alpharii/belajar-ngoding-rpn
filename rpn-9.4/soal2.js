16db3ed1d63874a6d4446e3fd017ddbd80f92916
alpharii <alphari404@gmail.com>
2024-03-10 20:41:29 +0700

    nomor 1 selesai

---
+++ rpn-9.4/soal2.js
@@ -0,0 +1,12 @@
+//DILARANG MENGGUNAKAN METHOD SORT, PELAJARI ALGORITMA SORTING YANG ADA DI GOOGLE
+//saran sih pake bubblesort walau tidak efisien tapi bagus buat belajar sorting
+function urutkanAbjad(str) {
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
