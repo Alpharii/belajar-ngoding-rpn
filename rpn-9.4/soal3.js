058ae376ac9c0f4a1d26fb58e81186862488c1d1
alpharii <alphari404@gmail.com>
2024-03-19 14:31:09 +0700

    fix no 3

--- rpn-9.4/soal3.js
+++ rpn-9.4/soal3.js
@@ -1,17 +1,27 @@
 //TIPS: gunakan method toUpperCase() dan toLowerCase()
 function tukarBesarKecil(kalimat) {
-  let hurufBesar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  let hurufKecil = hurufBesar.toLowerCase()
-  let result = ""
-  for(i=0; i<kalimat.length; i++){
-    for(j=0; j<hurufBesar.length; j++)
-      if(kalimat[i] === hurufBesar[j]){
+  let result = ''
+    for(i=0; i<kalimat.length; i++){
+      if(kalimat[i] === kalimat[i].toUpperCase()){
         result += kalimat[i].toLowerCase()
-      } else if (kalimat[i] === hurufKecil[j]){
+      } else if(kalimat[i] === kalimat[i].toLowerCase()){
         result += kalimat[i].toUpperCase()
-      } result += kalimat[i]
+      }
   } return result
-  } 
+} 
+
+  // let hurufBesar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  // let hurufKecil = hurufBesar.toLowerCase()
+  // let result = ""
+  // for(i=0; i<kalimat.length; i++){
+  //   for(j=0; j<hurufBesar.length; j++)
+  //     if(kalimat[i] === hurufBesar[j]){
+  //       result += kalimat[i].toLowerCase()
+  //     } else if (kalimat[i] === hurufKecil[j]){
+  //       result += kalimat[i].toUpperCase()
+  //     } result += kalimat[i]
+  // } return result
+  // } 
 
   
   // TEST CASES
@@ -20,3 +30,7 @@ function tukarBesarKecil(kalimat) {
   console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
   console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
   console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+
+
+
+ 
